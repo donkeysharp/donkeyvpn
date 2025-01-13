b98883978897f6efa102f2d613b9790a6e7329f5
Sergio Guillen <serguimant@gmail.com>
2025-01-12 20:50:41 -0400

    Add ability to get and update dynamodb records in helper struct

--- internal/aws/dynamodb.go
+++ internal/aws/dynamodb.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/labstack/gommon/log"
@@ -19,8 +20,13 @@ type DynamoDB struct {
 
 type DynamoDBItem interface {
 	ToItem() map[string]types.AttributeValue
-	PrimaryKey() string
-	RangeKey() string
+	PrimaryKey() map[string]types.AttributeValue
+	RangeKey() map[string]types.AttributeValue
+}
+
+type UpdateItem interface {
+	DynamoDBItem
+	UpdateExpression() (*expression.Expression, error)
 }
 
 func NewDynamoDB(ctx context.Context, tableName string) (*DynamoDB, error) {
@@ -48,6 +54,45 @@ func (d *DynamoDB) CreateRecord(item DynamoDBItem) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+func (d *DynamoDB) UpdateRecord(item UpdateItem) error {
+	expr, err := item.UpdateExpression()
+	if err != nil {
+		return err
+	}
+
+	output, err := d.client.UpdateItem(d.ctx, &dynamodb.UpdateItemInput{
+		TableName:                 &d.TableName,
+		Key:                       item.PrimaryKey(),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		UpdateExpression:          expr.Update(),
+		ReturnValues:              types.ReturnValueUpdatedNew,
+	})
+	if err != nil {
+		log.Errorf("Failed updating dynamodb record %v", err.Error())
+		return err
+	}
+
+	log.Infof("Record updated successfully. Output metadata ", output.ResultMetadata)
+	log.Infof("Record updated successfully. Output attributes ", output.Attributes)
+	return nil
+}
+
+func (d *DynamoDB) GetRecord(item DynamoDBItem) (map[string]types.AttributeValue, error) {
+	result, err := d.client.GetItem(d.ctx, &dynamodb.GetItemInput{
+		TableName: &d.TableName,
+		Key:       item.PrimaryKey(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(result.Item) == 0 {
+		log.Warnf("There is no data for this record: %v", item)
+		return nil, nil
+	}
+	return result.Item, nil
 }
 
 func (d *DynamoDB) ListRecords() ([]map[string]types.AttributeValue, error) {
