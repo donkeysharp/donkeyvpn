11e18919264ac605aa3aa870b17d93b698ca29e8
Sergio Guillen <serguimant@gmail.com>
2025-01-13 22:34:32 -0400

    Remove unused fields from ProcessorShared struct

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -1,14 +1,11 @@
 package processor
 
 import (
-	"github.com/donkeysharp/donkeyvpn/internal/aws"
 	"github.com/donkeysharp/donkeyvpn/internal/telegram"
 )
 
 type ProcessorShared struct {
 	Client *telegram.Client
-	asg    *aws.AutoscalingGroup
-	table  *aws.DynamoDB
 }
 
 type Processor interface {
